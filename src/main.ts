66c85a781961403eb8ba6859e92488bda2051f03
Alexander <oleksandr.dudko@customertimes.com>
2016-09-05 18:49:44 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { disableDeprecatedForms, provideForms } from '@angular/forms';
+
+import { bootstrap } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+import { AppComponent } from './app/app.component';
+
+if (process.env.ENV === 'production') {
+    enableProdMode();
+}
+
+bootstrap(AppComponent, [
+    disableDeprecatedForms(),
+    provideForms()
+]);
